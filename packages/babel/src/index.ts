43fc59286eb76684559e455bbc1c525692cc90c2
peterhs.kang <peterhs.kang@gmail.com>
2021-07-27 23:05:46 +0900

    - fix ts-node paths issue using tsconfig-path

--- packages/babel/src/index.ts
+++ packages/babel/src/index.ts
@@ -1,5 +1,5 @@
-import hello from "./lib/hello";
-import my from "./lib/my";
-import world from "./lib/world";
+import hello from "@/lib/hello";
+import my from "@/lib/my";
+import world from "@/lib/world";
 
 export { hello, my, world };
